6303dcae76cd3a4b5f18696ca947c410d4c5e801
1070020 <1070020@isep.ipp.pt>
2025-03-10 21:50:02 +0000

    Criado o projeto maven com o esqueleto da classe principal DemoORM.

--- DemoORM/src/main/java/isep/eapli/demo_orm/DemoORM.java
+++ DemoORM/src/main/java/isep/eapli/demo_orm/DemoORM.java
@@ -4,7 +4,7 @@ import isep.eapli.demo_orm.apresentacao.MainMenu;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class DemoORM {
     public static void main(String[] args) {
         MainMenu.mainLoop();
     }
